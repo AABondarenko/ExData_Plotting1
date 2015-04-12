db7bfd950f9b4a350893794430b56da97ffa6e12
Andrey Bondarenko <bondarenko.a.a.7@gmail.com>
2015-04-12 22:46:11 +0300

    commit1

---
+++ plot1.R
@@ -0,0 +1,4 @@
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
